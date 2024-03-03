0c3525276636c2efb82e06b1ac9b197df6405ec3
GeorgeTech20 <giorgifrancknr@gmail.com>
2024-03-02 22:42:51 -0600

    mejoras

---
+++ main.js
@@ -0,0 +1,98 @@
+const ROCK = "piedra";
+const PAPER = "papel";
+const SCISSORS = "tijera";
+
+const TIE = 0;
+const WIN = 1;
+const LOST = 2;
+
+let isPlaying = false;
+
+const rockBtn = document.getElementById("piedra");
+const paperBtn = document.getElementById("papel");
+const scissorsBtn = document.getElementById("tijera");
+const resultText = document.getElementById("start-text");
+const userImg = document.getElementById("user-img");
+const machineImg = document.getElementById("machine-img");
+
+rockBtn.addEventListener("click", () => {
+  play(ROCK);
+});
+paperBtn.addEventListener("click", () => {
+  play(PAPER);
+});
+scissorsBtn.addEventListener("click", () => {
+  play(SCISSORS);
+});
+
+function play(userOption) {
+  if(isPlaying) return;
+
+    isPlaying = true;
+
+    userImg.src = "/public/img/" + userOption + ".webp";
+
+    resultText.innerHTML = "Eligiendo!";
+
+    const interval = setInterval(function(){
+        const machineOption = calcMachineOption();
+        machineImg.src = "/public/img/" + machineOption + ".webp";
+    }, 200);
+
+    setTimeout(function () {
+
+        clearInterval(interval);
+
+        const machineOption = calcMachineOption();
+        const result = calcResult(userOption, machineOption);
+
+        machineImg.src = "/public/img/" + machineOption + ".webp";
+
+        switch (result) {
+            case TIE:
+                resultText.innerHTML = "Empate!";
+                break;
+            case WIN:
+                resultText.innerHTML = "Ganaste!";
+                break;
+            case LOST:
+                resultText.innerHTML = "¡Perdiste!";
+                break;
+        }
+        isPlaying = false;
+    }, 2000);
+}
+
+function calcMachineOption() {
+    const number = Math.floor(Math.random() * 3);
+    switch (number) {
+        case 0:
+            return ROCK;
+        case 1:
+            return PAPER;
+        case 2:
+            return SCISSORS;
+    }
+}
+
+function calcResult(userOption, machineOption) {
+    if (userOption === machineOption) {
+        return TIE;
+
+    } else if (userOption === ROCK) {
+
+        if (machineOption === PAPER) return LOST;
+        if (machineOption === SCISSORS) return WIN;
+
+    } else if (userOption === PAPER) {
+
+        if (machineOption === SCISSORS) return LOST;
+        if (machineOption === ROCK) return WIN;
+
+    } else if (userOption === SCISSORS) {
+
+        if (machineOption === ROCK) return LOSTWW   ;
+        if (machineOption === PAPER) return WIN;
+
+    }
+}
